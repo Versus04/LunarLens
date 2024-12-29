8e40173e1d0ee0aa3abcd053b9c03a2a245cea89
Versus04 <2shubhambind@gmail.com>
2024-12-30 00:20:39 +0530

    implemented the navigation bar

--- app/src/main/java/com/example/lunarlens/data/remote/api/apodApi.kt
+++ app/src/main/java/com/example/lunarlens/data/remote/api/apodApi.kt
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit
 
 private const val BASE_URL = "https://api.nasa.gov/planetary/"
 interface apodApi {
+
     @GET("apod")
     suspend fun getapod(
         @Query("api_key") api_key : String = Constants.api_key
